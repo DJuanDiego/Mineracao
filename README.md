958aa885618c81b577325877630d5b7535e6339d
DJuanDiego <juan.martins.cruz@gmail.com>
2020-06-28 17:26:49 -0300

    Questão 1 completa e outras alterações.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## A partir dos dados públicos da Universidade Federal Rural do Semi-Árido sobre disciplinas e professores apresente (se possível em gráficos) as respostas para as seguintes questões:
 
-### 01 - Quantas disciplinas foram ofertadas pela Universidade desde o início do funcionamento até o período 2019.2?
+### 01 - Quantas disciplinas foram ofertadas pela Universidade desde o início do funcionamento (2007) até o período 2019.1?
 
 
 ### 02 - Quais as disciplinas mais ofertadas da UFERSA?
--- bin/Q01.sh
+++ bin/Q01.sh
@@ -8,4 +8,26 @@
 N_Disc=$(wc -l src/Horarios.csv)
 
 # Escreve a mensagem num arquivo de texto.
-echo "A Universidade ofertou $N_Disc disciplinas entre 2009 e 2019" > output/Q1.txt
+echo "A Universidade ofertou $N_Disc disciplinas entre 2007 e 2019" > output/Q1.txt
+
+# Total de disciplinas por período
+cut -d, -f 3 < src/Horarios.csv | 
+    sed -e 's/"//g' | 
+    sort --numeric-sort | 
+    uniq -c | 
+    sed 's/^[^0-9]*//g' | 
+    awk '{print $2,$1}' | 
+    sort --numeric-sort | 
+    sed 's/ /,/g' > output/Q1_Periodo.csv
+
+# Total de disciplinas por ano
+cut -d, -f 3 < src/Horarios.csv | 
+    tr . \\t | 
+    sed -e 's/"//g' | 
+    cut -f 1 | 
+    sort --numeric-sort | 
+    uniq -c | 
+    sed 's/^[^0-9]*//g' | 
+    awk '{print $2,$1}' | 
+    sort --numeric-sort |
+    tr ' ' ',' > output/Q1_Ano.csv
